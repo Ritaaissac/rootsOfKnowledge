49da73e78180557175470aece889e613ba8cd240
Lívia Lopes Nóbrega <20231101110031@ifrn.local>
2026-01-22 07:35:43 -0300

    Inicialização do projeto

---
+++ backend/src/main/java/com/ifrn/rok/RokApplication.java
@@ -0,0 +1,13 @@
+package com.ifrn.rok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ifrn/rok/RokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifrn.rok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
