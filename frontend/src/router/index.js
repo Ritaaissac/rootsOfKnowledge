6391dce1d0175835858fc305056c2704f22ac948
Raissa Kelly Brito de Araújo <20231101110069@ifrn.local>
2026-01-27 15:58:14 -0300

    Eu criei as páginas para cadastrar um genero e ver os generos e adicionei em router, porém, não está conectado com o backend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import NovoAutor from '../views/NovoAutor.vue'
+import NovoGenero from '../views/NovoGenero.vue'
+import VerGenero from '@/views/VerGenero.vue'
 
 const routes = [
   {
@@ -13,6 +15,17 @@ const routes = [
     name: 'NovoAutor',
     component: NovoAutor
   },
+  {
+    path: '/novo-genero',
+    name: 'NovoGenero',
+    component: NovoGenero
+  },
+
+  {
+    path: '/ver-genero',
+    name: 'VerGenero',
+    component: VerGenero
+  }
 ]
 
 const router = createRouter({
