93b5e8965ae5394620a7e7c13dc03b15356def21
livialop <livvialn7@gmail.com>
2026-01-27 10:14:46 -0300

    rota para novo autor, ainda não funcional

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import NovoAutor from '../views/NovoAutor.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  }
+  },
+  {
+    path: '/novo-autor',
+    name: 'NovoAutor',
+    component: NovoAutor
+  },
 ]
 
 const router = createRouter({
