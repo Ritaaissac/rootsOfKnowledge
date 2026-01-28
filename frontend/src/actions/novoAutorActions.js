68083b9550a303f488d4be8991a9523de85544f5
livialop <livvialn7@gmail.com>
2026-01-28 09:45:32 -0300

    rota de adicionar autor corrigida e funcionando

--- frontend/src/actions/novoAutorActions.js
+++ frontend/src/actions/novoAutorActions.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env?.VITE_API_BASE_URL || ''
+    baseURL: import.meta.env?.VITE_API_BASE_URL || 'http://127.0.0.1:8080'
 })
 
 export const createNovoAutor = async (autorData) => {
-    const params = new URLSearchParams();
-    params.append('nome', autorData.nome);
-    params.append('portfolioAutor', autorData.portfolioAutor);
-    const response = await api.post('/novoautor', params);
+    const response = await api.post('/novoautor', autorData);
     return response.data;
 }
